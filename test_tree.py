2cf0eeaddb0ace05827d67d0a23d7db86bcdbeb9
b <yatsinaba@gmail.com>
2020-02-15 17:09:58 +0200

    finished tesk and added couple of comments

--- test_tree.py
+++ test_tree.py
@@ -1,10 +1,9 @@
 from unittest import TestCase
 
+
 class Test(TestCase):
     def test_tree(self):
-
-        expected_output = \
-"""asciitree
+        expected_output = """asciitree
  +-- sometimes
  |   +-- you
  +-- just
@@ -15,10 +14,15 @@ class Test(TestCase):
  +-- in
      +-- your
          +-- terminal"""
+        from tree import build_tree, print_node
+        import io
+        from contextlib import redirect_stdout
 
-        text = "(asciitree (sometimes you) (just (want to draw)) trees (in (your terminal)))"
-        from tree import Tree
-        example_tree, list = Tree.fromString(text)
-        # print("\n")
-        # print(expected_output)
-        assert example_tree.show() == expected_output
+        bulded_tree = build_tree('(asciitree (sometimes you) (just (want to draw)) trees (in (your terminal)))')
+        f = io.StringIO()
+        with redirect_stdout(f):
+            print_node(bulded_tree)
+
+        output = f.getvalue()
+
+        assert output == (expected_output + '\n')
--- tree.py
+++ tree.py
@@ -1,84 +1,100 @@
+"""Python 3.8.1 implementation `ascitree`
+
+to run default example type: python tree.py
+to run parse custom tree: python tree.py --str "(String what (should be parsed example))"
+
+"""
+
 import argparse
 
-parser = argparse.ArgumentParser(description='скрипт, який приймає на вхід текстовий опис дерева і друкує його так, як утиліта asciitre')
+parser = argparse.ArgumentParser(
+    description='Cкрипт, який приймає на вхід текстовий опис дерева і друкує його так, як утиліта asciitre')
 
-parser.add_argument('--str', default='(asciitree (sometimes you) (just (want to draw)) trees (in (your terminal)))', help="Build tree and show it from string. default example: \
+parser.add_argument('--str', default='(asciitree (sometimes you) (just (want to draw)) trees (in (your terminal)))',
+                    help="Build tree and show it from string. default example: \
 (asciitree (sometimes you) (just (want to draw)) trees (in (your terminal)))")
 
-class Tree:
-    def __init__(self, value):
-        self.value = value
-        self.parent = None
-        self.children = []
 
-    def __iadd__(self, child):
-        child.parent = self
-        self.children.append(child)
-        return self
-
-    def fromString(string):
-        return Tree.fromList(string.split())
-
-    def fromList(list):
-        itemList = list[0].lstrip('()')
-        itemList = itemList.rstrip('()')
-        node = Tree (itemList)
-        list = list[1:]
-
-        while len(list) > 0 and "(" in list[0]:
-            child, list = Tree.fromList(list)
-            child.value = child.value.lstrip('()')
-            node += child
-
-        endlist = list
-        itemAddedCount = 0
-
-        while len(endlist) > 0 and "(" not in endlist[0]:
-            node += Tree(endlist[0].rstrip('()'))
-            itemAddedCount += 1
-
-            if ")" in endlist[0]:
-                return node, list[itemAddedCount:]
-            endlist = endlist[1:]
-
-        return node, list[1:]
-
-    def show(self,lvl=0):
-        lvl +=1
-
-        if lvl == 1:
-            print(self.value + " " + str(lvl))
-        elif lvl == 2:
-            print("+",end='')
-            print("--", end='')
-            print(" " + self.value + " " + str(lvl))
-        else:
-            print("|", end='')
-
-            for i in range(1, lvl-2):
-                print(" ", end='')
-
-            for i in range(1, (lvl-2)*3+1):
-                print(" ", end='')
-
-            print("+--", end='')
-
-            if self.parent != None:
-                print(" ",end='')
-            print(self.value + " " + str(lvl))
-
-        for item in self.children:
-            item.show(lvl)
+class Node:
+    def __init__(self, data, child):
+        self.value = data
+        self.child = child
 
     def __repr__(self):
-        return '{}{}'.format(self.value, ': {}'.format(self.children) if self.children else '')
+        return '{}{}'.format(self.value, ': {}'.format(self.child) if self.child else '')
+
+
+def print_node(node, depth=0, no_bar=False):
+    if depth != 0:
+        tab = ' '
+
+        if depth != 1:
+            tab += ' ' if no_bar else '|'
+            tab += ' ' * 3 + ' ' * 4 * (depth - 2)
+
+        tab += '+-- '
+        print('{}{}'.format(tab, node.value))
+    else:
+        print(node.value)
+
+    if node.child:
+        length = len(node.child)
+        for i in range(0, length):
+            no_bar_decided = (i == length - 1 and depth == 0)
+            print_node(node.child[i], depth + 1, no_bar or no_bar_decided)
+
+
+def build_leaf(string):
+    return Node(string, None)
+
+
+def build_node(string):
+    s = string[1:-1]
+    elements = []
+    seen_open_bracket = 0
+    tmp = str()
+
+    if string[0] != '(' or string[-1] != ')':
+        return build_leaf(string)
+
+    for el in s:
+        if el == ' ' and seen_open_bracket == 0:
+            elements.append(tmp) if len(tmp) > 0 else 0
+            tmp = str()
+            continue
+
+        tmp = tmp + el
+        seen_open_bracket += 1 if el == '(' else 0
+
+        if el == ')':
+            seen_open_bracket -= 1
+            if seen_open_bracket < 0:
+                raise NameError("Format error")
+            if seen_open_bracket == 0:
+                elements.append(tmp)
+                tmp = str()
+                continue
+
+    elements.append(tmp) if len(tmp) > 0 else 0
+
+    head = elements[0]
+    elements = elements[1:]
+    if len(elements) == 0:
+        return build_leaf(head)
+
+    return Node(head, list(map(build_node, elements)))
+
+
+def build_tree(input_string):
+    if len(input_string) <= 2:
+        raise NameError('String too short')
+    if input_string[0] != '(' or input_string[-1] != ')':
+        raise NameError('Format error')
+
+    return build_node(input_string)
+
 
 if __name__ == "__main__":
-
     args = parser.parse_args()
-
-    text = args.str
-    example_tree, list = Tree.fromString(text)
-    print(example_tree)
-    example_tree.show()
-
+    tree_from_str = build_tree(args.str)
+    print_node(tree_from_str)
